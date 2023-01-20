03ddb2e4800620f4e49d527129a67160c772fd3c
Zeel Sutariya <zeel.sutariya@dcmail.ca>
2023-01-20 11:16:17 -0500

    app.js File Updated with a article para

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,28 @@
         });
     }
 
+    // STEP 3
+    let MainContent = document.getElementsByTagName("main")[0];
+    let MainParagraph = document.createElement("p");
+
+    // STEP 5
+    //MainParagraph.setAttribute("id", "MainParagraph");
+    //MainParagraph.setAttribute("class", "mt-3");
+    //MainParagraph.textContent = "This is the main Paragraph!";
+
+    let FirstString = "This is";
+    let SecondString = `${FirstString} the Main Paragraph.`;
+    MainParagraph.textContent = SecondString;
+
+    //STEP 6
+    MainContent.appendChild(MainParagraph);
+    let Article = document.createElement("article");
+    let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
+    Article.setAttribute("class", "container");
+    Article.innerHTML = ArticleParagraph;
+    // DocumentBody.appendChild(Article);
+    document.body.appendChild(Article);
+
     function DisplayProductsPage(){}
     function DisplayServicesPage(){}
     function DisplayContactPage(){}
