9ee34f68e5a48d9bdc980c5a3fe257e2b0a4336c
PSZwebd6201 <prerakkumar0710@gmail.com>
2023-02-17 22:04:20 -0500

    ICE 3

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,61 +1,53 @@
-"use strict";
+(function (core){
 
-(function (core) {
-    class Contact {
-        constructor(fullName, contactNumber, emailAddress) {
+
+    class Contact{
+        constructor(fullName,contactNumber,emailAddress){
             this.FullName = fullName;
             this.ContactNumber = contactNumber;
             this.EmailAddress = emailAddress;
         }
-
-        set FullName(fullName) {
-            this.m_fullName = fullName;
-
+        set FullName(fullName){
+            this.m_fullname = fullName;
         }
 
-        get FullName() {
-            return this.m_fullName;
+        get FullName(){
+            return this.m_fullname;
+        }
+        set ContactNumber(contactNumber)
+        {
+            this.m_contactnumber = contactNumber;
+        }
+        get ContactNumber(){
+            return  this.m_contactnumber;
+        }
+        set EmailAddress(emailAddress)
+        {
+            this.m_emailaddress = emailAddress;
+        }
+        get EmailAddress(){
+            return  this.m_emailaddress;
+        }
+        toString(){
+            return `FullName:${this.FullName} \n Contact Number:${this.ContactNumber} \n Email Address :${this.EmailAddress} `;
         }
 
-        set ContactNumber(contactNumber) {
-            this.m_contactNumber = contactNumber;
-
-        }
-
-        get ContactNumber() {
-            return this.m_contactNumber;
-        }
-
-        set EmailAddress(emailAddress) {
-            this.m_emailAddress = emailAddress;
-
-        }
-
-        get EmailAddress() {
-            return this.m_emailAddress;
-        }
-
-        toString() {
-            return `Full Name: ${this.FullName}\n Contact Number: $(this.ContactNumber}\n Email Address: ${this.EmailAddress}`;
-        }
-
-        serialize() {
-            if (this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== "") {
-                return `${this.FullName}, ${this.ContactNumber}, ${this.EmailAddress}`;
+        serialize(){
+            if(this.FullName !==""&& this.ContactNumber!== ""&& this.EmailAddress !== ""){
+                return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
             }
-
-            console.error("One or more of the properties of the Contact object are missing or invalid");
-
+            console.error("one or more of the contact attribute is empty or missing");
+            return null;
         }
-
-        deserialize(data) {
+        deserialize(data){
             let propertyArray = data.split(",");
-            this.FullName = propertyArray[0];
-            this.ContactNumber = propertyArray[1];
-            this.EmailAddress = propertyArray[2];
-
+            this.FullName  = propertyArray[0];
+            this.ContactNumber  = propertyArray[1];
+            this.EmailAddress  = propertyArray[2];
         }
 
     }
-    core.Contact = Contact;
-})(core || (core = {}));
+
+    core.Contact=Contact;
+
+})(core||(core={}));
